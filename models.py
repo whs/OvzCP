127d0b603972819ffd70c3d69c702c6c238753dc
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2010-02-24 21:39:16 +0700

    Added VarnishBackend VarnishConf in models

--- models.py
+++ models.py
@@ -5,9 +5,22 @@ sqlhub.processConnection = connectionForURI("sqlite://"+os.path.join(os.getcwd()
 class VM(SQLObject):
 	veid = IntCol()
 	owner = StringCol(default=None)
+	varnish_backend = MultipleJoin('VarnishBackend')
 	@property
 	def vz(self):
 		return openvz.VM(self.veid)
 
+class VarnishBackend(SQLObject):
+	name = StringCol()
+	vm = ForeignKey('VM')
+	port = IntCol()
+	cond = MultipleJoin('VarnishCond')
+
+class VarnishCond(SQLObject):
+	hostname = StringCol()
+	backend = ForeignKey('VarnishBackend')
+
 if __name__ == "__main__":
-	VM.createTable()
+	VM.createTable(True)
+	VarnishBackend.createTable(True)
+	VarnishCond.createTable(True)
