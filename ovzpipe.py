8481a5365d3cb95dd34d795dfe85b716362bd7ef
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2010-06-20 13:18:24 +0700

    - New theme from @WHHdotexe - Fixed a critical bug that ovzpipe.py use 100% cpu   http://stackoverflow.com/questions/3062134/named-pipe-is-using-100-cpu

--- ovzpipe.py
+++ ovzpipe.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
-import os
-while True:
-	try:
-		I = raw_input().strip().split()
-	except EOFError:
-		continue
-	if I[0] in ("start", "stop", "restart"):
-		os.system("vzctl "+I[0]+" "+I[1])
+import os, sys, time
+def readline():
+	out = ""
+	while True:
+		b = sys.stdin.read(1)
+		out += b
+		if b == "\n": break
+		time.sleep(0.01)
+	return out
+
+if __name__ == "__main__":
+	while True:
+		I = readline().strip().split()
+		if I[0] in ("start", "stop", "restart"):
+			os.system("vzctl "+I[0]+" "+I[1])
